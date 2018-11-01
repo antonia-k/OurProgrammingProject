c0e2d6358e348c6007be9fa72bd62eb66e26fd8c
Dr. Paul Kellerwessel <kellerwp@Dr-Paul-Kellerwessels-MacBook-Pro.local>
2018-11-01 10:07:58 +0100

    JS to HTML

--- JS/marketplace.js
+++ JS/marketplace.js
@@ -1,3 +1,50 @@
+//var jobs = JSON.parse(localStorage.getItem("jobs"));
+class JobPosting{
+  constructor(title,description,qualifications,jobLocation,linkToWebsite,linkToContact){
+      this.jobTitle = title;
+      this.jobDescription = description;
+      this.qualifications = qualifications;
+      this.jobLocation = jobLocation; 
+      //this.image = image;
+      this.linkToWebsite = linkToWebsite;
+      this.linkToContact = linkToContact;
+  }
+} 
+
+var jobs = JSON.parse(localStorage.getItem("jobs"));
+
+if(jobs === null){
+  jobs = [];
+  jobs.push(new JobPosting("Front-End Developer at Orsted", "Designing a Website for the sale of energy", "CSS, HTML, JavaScript", "Copenhagen", "https://orsted.com/en/About-us", "https://orsted.com/en/About-us"));
+  jobs.push(new JobPosting("Back-End Developer at E.ON", "Developing Database for Energy sources", "Java, Ruby, Python", "Aarhus", "https://www.eon.dk/privat.html", "https://www.eon.dk/privat.html"));
+  jobs.push(new JobPosting("Web Designer for IBM", "Designing a Pretty Website for IBM", "Photoshop", "Odense", "https://www.ibm.com/dk-da/", "https://www.ibm.com/dk-da/"));
+  jobs.push(new JobPosting("Back-End Developer at REWE", "Developing Database for Food Shopping", "Java, Ruby, Python", "Copenhagen", "https://www.rewe.de/", "https://www.rewe.de/"));
+}
+
+
+//creating an HTML from the JS objects
+function createHTML(job){
+  return "<div class='card'>"+
+                "<h1 class='jobtitle'>" + job.jobTitle + "</h1>"+
+                "<p class='jobDescription'>" + job.jobDescription + "</p>"+
+                "<p class='qualifications'>" + job.qualifications + "</p>"+
+                "<p class='location' data-location-type=" + job.location + "></p>"+
+                "<p><button onclick='window.location.href='" + job.linkToWebsite + ">Company Website</button></p>"+
+                "<p><button onclick='window.location.href='" + job.linkToContact + ">Contact</button></p>"+
+              "</div>";
+}
+
+var content = "";
+for(var i =0; i<jobs.length; i++){
+    content += createHTML(jobs[i]);
+}
+
+document.getElementById('searchDivs').innerHTML = content;
+
+
+//local storage holen. das mit dem create html definieren mit loop und dann wird durch createHTML die funktion "gerufen", brauche noch window.loc.ref zum marketplace
+//createHTML();
+
 //Search function for the Job Cards
 function searchFunction(){
   //Declare variables - getting values from the search box
@@ -20,6 +67,8 @@ function searchFunction(){
 
 }
 
+
+
 // filter function by location
 function filterFunction(checkbox){
   var jobAds = document.getElementById("searchDivs");
@@ -38,19 +87,19 @@ function filterFunction(checkbox){
     }
   }
 
-
+  
 
 /*
 //creating an HTML from the JS objects
-    createHTML(){
+    function createHTML(){
 
-        var html = "<div class='card'><h1 class='jobtitle'>" + this.title + "</h1><p class='jobDescription'>" + this.jobDescription + "</p><p class='qualifications'>" + this.qualifications + "</p><p class='location' data-location-type=" + this.location + "></p><p><button onclick='window.location.href=`" + this.linkToWebsite + "`>Company Website</button></p><p><button onclick='window.location.href=`" + this.linkToContact "`>Contact</button></p></div>";
+        var html = "<div class='card'><h1 class='jobtitle'>" + this.title + "</h1><p class='jobDescription'>" + this.jobDescription + "</p><p class='qualifications'>" + this.qualifications + "</p><p class='location' data-location-type=" + this.location + "></p><p><button onclick='window.location.href=`" + this.linkToWebsite + "`>Company Website</button></p><p><button onclick='window.location.href=`" + this.linkToContact + "`>Contact</button></p></div>";
 
         return html;
     }
 
 
-}
+
 
 var postings = [new JobPosting("First"), new JobPosting("Second")];
 
