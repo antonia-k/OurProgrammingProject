68cf299c2b0bbc537d22c1422e0a90478a09749f
Dr. Paul Kellerwessel <kellerwp@Dr-Paul-Kellerwessels-MacBook-Pro.local>
2018-11-12 13:48:19 +0100

    editProfile edited

--- editProfile.js
+++ editProfile.js
@@ -1,5 +1,10 @@
 function saveChanges() {
 
+    loggedInUser.firstname = document.getElementById("firstName").value;
+    loggedInUser.lastname = document.getElementById("lastName").value;
+    loggedInUser.dateOfBirth = document.getElementById("qualifications").value;
+    loggedInUser.qualifications = document.getElementById("qualifications").value;
+    loggedInUser.description = document.getElementById("description").value;
 
     window.location.href="./UserProfile.html";
 }
